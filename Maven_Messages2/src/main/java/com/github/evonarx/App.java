6a2ea963c64ea92d19ed84bf439e750aa8346297
EVonarx <48631478+EVonarx@users.noreply.github.com>
2020-03-05 09:35:47 +0100

    first commit

---
+++ Maven_Messages2/src/main/java/com/github/evonarx/App.java
@@ -0,0 +1,15 @@
+package com.github.evonarx;
+
+public class App {
+    public String getMessage() {
+        return "Hello World!";
+    }
+    
+    public String getMessage1() {
+        return "Great message!";
+    }
+    
+    public String getMessage2() {
+        return "This is really a great message!";
+    }
+}
